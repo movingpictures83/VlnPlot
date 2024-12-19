e7ca0726d66643cfcdad81caaeb36ba6033a64a6
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-12-19 17:56:42 -0500

    Initial

---
+++ VlnPlotPlugin.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+source("RIO.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+    pdf(outputfile)
+    pbmc <- readRDS(paste(pfix, parameters["rdsfile", 2], sep="/"))
+    feat <- readSequential(paste(pfix, parameters["features", 2], sep="/"))
+    VlnPlot(pbmc, features=feat, ncol=length(feat))
+}
+
+
